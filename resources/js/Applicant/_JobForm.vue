47d57ea8a1f70b7c8d4870b424571bf396fd0e14
Jeramy <jdramos@mmsu.edu.ph>
2023-07-19 14:00:23 +0800

    'Maku'

--- resources/js/Applicant/_JobForm.vue
+++ resources/js/Applicant/_JobForm.vue
@@ -109,6 +109,7 @@ export default {
             },
 
             editor: ClassicEditor,
+            
             editorConfig: {
                 toolbar: [ 'bold', 'italic', 'link', '|', 'numberedList', 'bulletedList', '|', 'undo', 'redo' ]
             },
